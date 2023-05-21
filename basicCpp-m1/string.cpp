56178583ca243e39117076c2f85eec8e9181340c
unknown <nazmulnobel000@gmail.com>
2023-05-21 19:42:46 +0600

    first module done

---
+++ basicCpp-m1/string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char s[100];
+    // cin>>s;
+    // cout<<s;
+    cin.getline(s,100);
+    // cout<<strlen(s);
+    cout<<s;
+    return 0;
+}
